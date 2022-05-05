de7ae25a841b55b7a0d8c6bf077152624ef127e3
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-05-05 18:24:44 -0300

    Etapa 2 concluida e inicio da 3

---
+++ script.js
@@ -0,0 +1,17 @@
+function login() {
+  const btnLogin = document.getElementById('btn-login');
+//   const emailInput = document.querySelectorAll('.trybewarts-login')[0];
+//   const senhaInput = document.querySelectorAll('.trybewarts-login')[1];
+
+  btnLogin.addEventListener('click', () => {
+    // if (emailInput.innerText === 'tryber@teste.com' && senhaInput.innerText === '123456') {
+    //   alert('Olá, Tryber!');
+    //   if (emailInput.innerText !== 'tryber@teste.com' && senhaInput.innerText !== '123456') {
+    //     alert('Email ou senha inválidos');
+    //   }
+    // }
+    alert('teste');
+  });
+}
+
+login();
